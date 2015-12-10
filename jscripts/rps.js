04a65ee0af4df482c9527650189e2bade29dfd65
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-12-09 21:02:33 -0500

    stop button is bound when the game starts

--- jscripts/rps.js
+++ jscripts/rps.js
@@ -15,7 +15,17 @@ var computerGuess = 0;
     userGuess = $(this).data('value');
     computerGuessNumber = Math.floor(Math.random()*rockPaperScissors.length)
     computerGuess = rockPaperScissors[computerGuessNumber];
-  
+    
+    //bind the stop button here, once the game starts
+    $("#playAgain").on("click", function(e){
+      e.preventDefault();
+      $("#myModal").modal('show');
+      resetGame();
+    });
+
+    //th idea here is to change the stop button color to red
+    $("#playAgain").removeClass("btn-default");
+    $("#playAgain").addClass("btn-danger");
     $('#computer-guess').html("My guess is " + computerGuess);
    
     if (userGuess != computerGuess) {
@@ -30,11 +40,13 @@ var computerGuess = 0;
     }
   });
 
-  $("#playAgain").on("click", function(e){
-    e.preventDefault();
-    $("#myModal").modal('show');
-    resetGame();
-  });
+  //unbind the end when the game begins, bind it when the game stars
+  // and have the button change to btn-danger
+  // $("#playAgain").on("click", function(e){
+  //   e.preventDefault();
+  //   $("#myModal").modal('show');
+  //   resetGame();
+  // });
 
   function increaseComputersScore() {
     computerScore++;
@@ -59,10 +71,18 @@ var computerGuess = 0;
     $('#round-number').html(0);
     $('#computer-guess').html("My guess");
     $('#who-wins').html("Let's play again!")
+    //the end button is changed back to default
+    $("#playAgain").removeClass("btn-danger");
+    $("#playAgain").addClass("btn-default");
     userGuess ="";
     userScore = 0;
     computerScore = 0;
     roundNumber = 0;
   }
+
+  //here the html for the guess text fades in
+  // function htmlFadeIn(){
+  //   (.guessText).fadeIn(1000)
+  // }
 
 });
