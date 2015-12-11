ad956d41ed8af5966b4a49e716bc28846eb5ee95
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-12-11 14:34:35 -0500

    created animation function, added fade in and fade out

--- jscripts/rps.js
+++ jscripts/rps.js
@@ -16,11 +16,9 @@ var computerGuess = 0;
     computerGuessNumber = Math.floor(Math.random()*rockPaperScissors.length)
     computerGuess = rockPaperScissors[computerGuessNumber];
     
-    //the idea here is to slide down the button that is selected
-    $(this).addClass("slideDown");
 
-
-    //bind the stop button here, once the game starts
+    //bind the stop button when rock, paper or
+    //scissors button is clicked
     $("#playAgain").on("click", function(e){
       e.preventDefault();
       $("#myModal").modal('show');
@@ -28,12 +26,24 @@ var computerGuess = 0;
     });
 
     //th idea here is to change the stop button color to red
+    //by changing class
     $("#playAgain").removeClass("btn-default");
     $("#playAgain").addClass("btn-danger");
 
-    //the idea here is to delay the time that the text is shown
-    //and fade it in
+    //the idea here is to fade the header text in and out
+    $('#gameHeader').fadeOut(0);
+    $("#gameHeader").html("Guess again!");
+    $("#gameHeader").css("color","#FF0DFF");
+    $("#gameHeader").fadeIn(1000);
+    $("#instructionsList").css("color","#E80C7A");
+
+
+
+    //the idea here is to fade out the text and fade it in
+    $('#computer-guess').fadeOut(0).fadeIn(2000);
     $('#computer-guess').html("My guess is " + computerGuess);
+    $('#computer-guess').css("color", "#FF530D");
+
    
     if (userGuess != computerGuess) {
       increaseUsersScore();
@@ -41,19 +51,34 @@ var computerGuess = 0;
       increaseComputersScore();
     }
 
-    if (roundNumber === 5) {
+    if (roundNumber === 6) {
       $("#myModal").modal('show');
       resetGame();
     }
   });
 
+  createAnimation();
+   
+
+  function createAnimation() {
+    $(".userChoice").on("mouseenter", function() {
+         $(this).addClass("slideDown");
+    }); 
+    $(".userChoice").on("mouseleave", function() {
+         $(this).removeClass("slideDown");
+    });  
+  }
 
   function increaseComputersScore() {
     computerScore++;
     roundNumber++;
     $('#computer-score').html(computerScore);
     $('#round-number').html(roundNumber);
+    $('#who-wins').fadeOut(0);
+    $('#who-wins').fadeIn(2000);
     $('#who-wins').html("I win");
+    $('#who-wins').css("color", "#FF530D");
+
   }
 
   function increaseUsersScore(){
@@ -61,7 +86,10 @@ var computerGuess = 0;
     roundNumber++;
     $('#user-score').html(userScore);
     $('#round-number').html(roundNumber);
-    $('#who-wins').html("You win")
+    $('#who-wins').fadeOut(0);
+    $('#who-wins').fadeIn(2000);
+    $('#who-wins').html("You win");
+    $('#who-wins').css("color", "#FF530D");
   }
 
   function resetGame() {
@@ -69,14 +97,17 @@ var computerGuess = 0;
     $('#computer-score').html(0);
     $('#round-number').html(0);
     $('#computer-guess').html("My guess");
+    $('#computer-guess').css("color", "black");
     $('#who-wins').html("Let's play again!")
+    $('#who-wins').css("color", "black");
+    $("#gameHeader").html("Play Rock, Paper, or Scissors");
+    $("#gameHeader").css("color","black");
     //the end button is changed back to default
     $("#playAgain").removeClass("btn-danger");
     $("#playAgain").addClass("btn-default");
-    //remove class slideDown from buttons
-    $(".userChoice").removeClass("slideDown");
     //unbind the stop button
     $("#playAgain").off("click");
+    $("#instructionsList").css("color","black");
     userGuess ="";
     userScore = 0;
     computerScore = 0;
